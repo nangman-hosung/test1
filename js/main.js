27cf2224b7a2c6b9543725cbbad123a7b54e4147
nangman-hosung <antonio0104@naver.com>
2023-01-21 20:55:59 +0900

    test1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const bars = document.querySelector('.header_bars');
+const menu = document.querySelector('.header_menu');
+const login = document.querySelector('.header_login');
+const toggleBtn = document.querySelector('#toggle')
+
+bars.addEventListener('click', function(){
+   menu.classList.toggle('active');
+   login.classList.toggle('active');
+});
